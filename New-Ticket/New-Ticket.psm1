fee101a2f404956b1ee4055133eb8ae8382d3690
Ben Dixon <ben.dixon@enable.com>
2019-09-05 09:28:50 +0100

    Ensure prefix is upper case

--- New-Ticket/New-Ticket.psm1
+++ New-Ticket/New-Ticket.psm1
@@ -1,9 +1,12 @@
 function New-Ticket([String] $reference, [String] $prefix = 'H') {
     if ($reference -match '^(\d*)\/(\d*)$') {
+        $prefix = $prefix.ToUpper()
+
         $ticketNumber = $Matches[1]
         $ticketPart = $Matches[2]
         $ticketRef = "$prefix$($ticketNumber)-$($ticketPart)"
         $filename = "$($ticketRef).md"
+        
         New-Item $ticketRef -ItemType Directory -ErrorAction SilentlyContinue | out-null
         Set-Location $ticketRef
         New-Item $filename -ItemType File -ErrorAction SilentlyContinue | out-null
