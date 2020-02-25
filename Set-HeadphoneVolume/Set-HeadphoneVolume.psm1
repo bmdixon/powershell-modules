9e1f23e30c6553fe9b83377d53c15a66b2096e85
Ben Dixon <ben.dixon@enable.com>
2020-02-25 17:00:33 +0000

    Add Jabra headphone configuration

--- Set-HeadphoneVolume/Set-HeadphoneVolume.psm1
+++ Set-HeadphoneVolume/Set-HeadphoneVolume.psm1
@@ -5,16 +5,21 @@ function Set-HeadphoneVolume() {
     Import-Module AudioDeviceCmdlets
     $HeadphonesID = "{0.0.0.00000000}.{349b91a8-939f-4bfe-b02c-f64bc642ccb1}"
     $HeadphonesName = "Headphones (AKG Y50BT Stereo)"
+    $JabraHeadphonesID = "{0.0.0.00000000}.{78be0308-b450-4955-84ca-9f1b01400d20}"
+    $JabraHeadphonesName = "Headphones (Jabra Elite 65t Stereo)"
 
     function HeadphonesAreDefault {
         $DefaultPlayback = Get-AudioDevice -Playback
 
-        return ($DefaultPlayback.ID -eq $HeadphonesID -or $DefaultPlayback.Name -eq $HeadphonesName)
+        return ($DefaultPlayback.ID -eq $HeadphonesID `
+                -or $DefaultPlayback.Name -eq $HeadphonesName `
+                -or $DefaultPlayback.ID -eq $JabraHeadphonesID `
+                -or $DefaultPlayback.Name -eq $JabraHeadphonesName)
     }
 
     If (-Not (HeadphonesAreDefault)) {
         Get-AudioDevice -List | Where-Object { $_.Type -eq "Playback" } | ForEach-Object {
-            if ($_.ID -eq $HeadphonesID -or $_.Name -eq $HeadphonesName) {            
+            if ($_.ID -eq $HeadphonesID -or $_.Name -eq $HeadphonesName -or$_.ID -eq $JabraHeadphonesID -or $_.Name -eq $JabraHeadphonesName ) {            
                 Set-AudioDevice -ID $_.ID | Out-Null        
             }
         }
