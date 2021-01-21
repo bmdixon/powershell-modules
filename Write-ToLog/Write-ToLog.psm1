017f4a3594047fddcc5bf63db792a56e5eb25488
Ben Dixon <ben.dixon@enable.com>
2021-01-21 20:23:18 +0000

    Write-ToLog now gets year for filename automatically

--- Write-ToLog/Write-ToLog.psm1
+++ Write-ToLog/Write-ToLog.psm1
@@ -1,9 +1,10 @@
 function Write-ToLog() {
     $date = Get-Date -format "yyyy-MM-dd"
+    $year = (Get-Date).Year
 
     $message = "$($date) | $($args)"
 
-    Add-Content -Value $message -Path "$($env:OneDriveCommercial)\\Build Automation\\2020.log"
+    Add-Content -Value $message -Path "$($env:OneDriveCommercial)\\Build Automation\\$($year).log"
 
     Write-Host "logged."
 }
